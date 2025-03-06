54d7e2aef70c4dc6acba3dc1ea97515f242184d1
Dhanush L <dhanush.l@valtech.com>
2025-03-06 14:47:30 +0530

    bugs

--- src/components/home.js
+++ src/components/home.js
@@ -3,12 +3,20 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-  return(
+  const UnusedVar = "I am not used anywhere"; 
+
+  console.log("Debug: Home component loaded"); 
+
+  return (
     <div className="home">
       <h1>Home</h1>
-      <p>Welcome to the Home page.</p>
+      <p style={{ color: 'red', fontSize: '20px' }}>Welcome to the Home page.</p>
+      
+      <img src="image.jpg" /> 
+
+      <p>Hardcoded Text: "This is not localized!"</p> 
     </div>
-  )
+  );
 }
 
 export default Home;
